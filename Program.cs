ec680faae75865bdb3b6553c080d4dc13eef509c
sippo <sippo@DESKTOP-4013P7G>
2024-03-16 17:42:54 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Lab7_h_ListT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> computers = new List<string> { "Dell", "Gateway", "Apple", "HP", "Acer" };
+            string[] compArr = new string[] { "Asus", "Sony", "Android" };
+            computers.AddRange(compArr);
+
+
+            computers.Sort();
+            Console.WriteLine("--------Ascending---------");
+            foreach (string c in computers)
+            {
+                Console.WriteLine(c);
+            }
+
+            computers.Remove("Android");
+
+            computers.Reverse();
+            Console.WriteLine("--------Descending---------");
+            foreach (string c in computers)
+            {
+                Console.WriteLine(c);
+            }
+
+        }
+    }
+}
